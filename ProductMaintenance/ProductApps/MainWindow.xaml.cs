c94adc5113db054be3ff2a52e102d97dc18d6ed5
KieranGough <kieran.gough@student.tafesa.edu.au>
2022-04-01 10:31:09 +1030

    Commit the branch feat_GST1.2

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,7 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.0m);
                 wrapChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25.0m + 5.0m);
+                gstTextBox.Text = Convert.ToString((cProduct.TotalPayment + 25.0m + 5.0m) * 1.1m);
             }
             catch (FormatException)
             {
@@ -51,6 +52,7 @@ namespace ProductApps
             totalPaymentTextBlock.Text = "";
             totalChargeTextBox.Text = "";
             wrapChargeTextBox.Text = "";
+            gstTextBox.Text = "";
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
